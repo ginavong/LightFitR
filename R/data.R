4ab9ee4dc9a7cbccf30effddd6eac8359ca9c4e1
Gina <77932267+ginavong@users.noreply.github.com>
2024-02-12 16:23:17 +0000

    Heliospectra metadata to package

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' Maximum number of events
+#'
+#' Maximum number of events programmable onto heliospectra
+#'
+#' @format Integer representing maximum allowable events
+#'
+"helio.eventLimit"
+
+#' heliospectra DYNA LEDs
+#'
+#' Data about the heliospectra DYNA LED channels
+#'
+#' @format A data frame with 9 rows and 3 columns:
+#' \describe{
+#'  \item{name}{Name of the LED channel}
+#'  \item{wavelength}{Wavelength of the LED channel}
+#'  \item{colour}{Colour of the LED channel}
+#'}
+#'
+#'@source <https://heliospectra.com/led-grow-lights/dyna/>
+#'
+"helio.dyna.leds"
