8f40b93869bb496ea16abee1ad1dbbe416990eff
Gina <77932267+ginavong@users.noreply.github.com>
2024-02-13 14:55:53 +0000

    internal.checkNAs
    Internal function to check the matrix for NAs. Split into its own function separate to helio.checkFormat. Unsure about the warning / error message

---
+++ R/internal.checkNAs.R
@@ -0,0 +1,19 @@
+#' Internal function to check matrix for NAs
+#'
+#' @inheritParams helio.checkFormat
+#'
+#' @return Boolean: TRUE = pass, FALSE = fail
+#' @export
+#'
+#' @examples
+internal.checkNAs = function(check_matrix){
+
+  naMessage = 'Your matrix contains NAs. The heliospectra cannot accept NAs as input \n' #May need to reword
+
+  if(sum(is.na(check_matrix)) > 0){
+    warning(naMessage) # IDK if error would be too strong here?
+    return(FALSE)}
+
+  else{return(TRUE)}
+
+}
