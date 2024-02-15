b4ae94a7b4252dfa2be62c7b2aba988598275929
Gina <77932267+ginavong@users.noreply.github.com>
2024-02-15 14:07:41 +0000

    create internal.closestIntensities
    First step of calculating intensities. Internal function

---
+++ R/internal.closestIntensities.R
@@ -0,0 +1,46 @@
+#' Internal function. Find the intensities corresponding to the closest irradiance match between intended and calibration.
+#'
+#' @param irradiance_matrix Matrix of intended irradiances. rows = leds and columns = events
+#' @param nEvents Number of events in schedule.
+#' @inheritParams internal.calibCombine
+#' @param peaks Vector of length 8 or 9. Containing wavelengths at which each LED peaks.
+#'
+#' @return Matrix of closest intensities, in the same format as `irradiance_matrix`
+#' @export
+#'
+#' @examples
+internal.closestIntensities = function(irradiance_matrix, nEvents, calibration_leds, calibration_wavelengths, calibration_intensities, calibration_irradiances, peaks=helio.dyna.leds$wavelength){
+
+  # Setup
+  calib = internal.calibCombine(calibration_leds, calibration_wavelengths, calibration_intensities, calibration_irradiances)
+
+  ## Checks
+  helio.checkFormat(irradiance_matrix)
+  helio.checkWhite(irradiance_matrix)
+  checkRange(irradiance_matrix, calib$led, calib$wavelength, calib$intensity, calib$irradiance)
+
+  # Calculate closest
+  closestIntensityMatrix = sapply(1:(nrow(helio.dyna.leds)-1), function(a){ # Go through each LED and calculate the closest we can get with calibration data
+    intendedSubset = c(as.numeric(irradiance_matrix[a,]))
+
+    criteria = (calib$led == helio.dyna.leds[a, 'wavelength']) & (round(calib$wavelength) == round(peaks[a]))
+    calibSubset = calib[criteria,] #subset calib data based on the led in question, and teh wavelength at the peak
+
+    # Closest intensity for each timepoint
+    closestIntensity = sapply(intendedSubset, function(i){
+
+      if(i==0){closest=0}
+      else{
+        closestIrradiance = which.min(abs(calibSubset$irradiance - i))[1] #Which is the 1st closest to intended irradiance?
+        closest = calibSubset[closest, 'intensity'] #Get the corresponding intensity
+      }
+
+      closest
+    })
+
+    closestIntensity
+  })
+
+  closestIntensityMatrix = t(closestIntensityMatrix)
+  return(closestIntensityMatrix)
+}
