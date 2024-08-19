790eeae9d4995098e8fb70ed09e56f6166de1c7d
Gina Vong <gina121vong@gmail.com>
2024-08-19 12:38:51 +0100

    Wrote export functions

---
+++ R/helio.csvSchedule.R
@@ -0,0 +1,22 @@
+#' Format regime_matrix for csv output that Heliospectra lights can parse
+#'
+#' @inheritParams write.helioSchedule
+#'
+#' @return Matrix formatted for Heliospectra lights csv
+#' @export
+#'
+#' @examples
+helio.csvSchedule = function(regime_matrix, filename){
+  LightFitR::helio.checkFormat(regime_matrix[-c(1:4),])
+
+  # Header
+  header = matrix(nrow=3, ncol=10)
+  header[1,] = c('Schedule.exported.from', filename, rep('', 8))
+  header[2,] = c('Lamp timestamp', format(sys.time(), "%Y-%m-%d %H:%M:%S"), rep('', 8))
+  header[3,] = c('', helio.dyna.leds$name)
+
+  # Schedule
+  schedule = t(regime_matrix[-c(2:4),])
+
+  return(rbind(header, schedule))
+}
---
+++ R/helio.json_schedule.R
@@ -0,0 +1,59 @@
+#' Format regime_matrix for json output that Heliospectra lights can parse
+#'
+#' @inheritParams write.helioSchedule
+#'
+#' @return Character in json format that Heliospectra can parse
+#' @export
+#'
+#' @examples
+helio.json_schedule = function(regime_matrix, filename){
+  LightFitR::helio.checkFormat(regime_matrix[-c(1:4),])
+
+  # Define subfunctions
+  timeRecipe = function(event_vector){ #Makes the timing part of the string when given input vector
+    paste('{\"hour\" : ', event_vector[2], ', \"minute\" : ', event_vector[3], ', \"second\" : ', event_vector[4], sep='')
+  }
+  lightRecipe = function(event_vector){ #Input: light intensity vector without time consideration - not sure if good idea - think light will sort that as long as rownames are sensible
+    paste(sapply(1:nrow(helio.dyna.leds), function(i){
+      paste('{\"wl\":', helio.dyna.leds[i, 'wavelength'], ',\"i\":', event_vector[4+i], "}", sep="")
+    }), sep="", collapse = ",")
+  }
+
+  # Define variables
+  nEvents = ncol(regime_matrix)
+
+  wlVec=paste(sapply(helio.dyna.leds[,'wavelength'], function(light){
+    paste("{\"wl\" : ", light, ",\"pwr\": 94 }", sep="")
+  }), collapse = ",\n")
+
+  # Header of json file
+  header = c("{",
+             paste('\"text\": \"', filename, '\",', sep=''),
+             "\"type\" : 0,",
+             "\"no_of_wave_lengths\" : 9,",
+             paste("\"no_of_events\": ", nEvents, ",", sep=""),
+             "\"wavelengths\" : [",
+             wlVec,
+             "],",
+             "\"events\":[")
+
+  # Make schedule part of input file
+  schedule = sapply(1:nEvents, function(event){
+
+    eventVec = regime_matrix[,event]
+
+    if(event < nEvents){
+      text=paste(timeRecipe(eventVec), ', \"intensities\" : [', lightRecipe(eventVec), ']},', sep="")
+    }
+    else { #last time point has different formatting (can't have a comma at the end of the string)
+      text=paste(timeRecipe(eventVec), ', \"intensities\" : [', lightRecipe(eventVec), ']}', sep="")
+    }
+
+    text
+  })
+
+  # Footer
+  footer = ']}'
+
+  return(c(header, schedule, footer))
+}
