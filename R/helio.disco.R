b99d6c62724b071d73dce1ef3e3f8f7c7731586a
Gina Vong <gina121vong@gmail.com>
2024-08-19 12:45:59 +0100

    Create helio.disco.R

---
+++ R/helio.disco.R
@@ -0,0 +1,35 @@
+#' Write a disco schedule for fun!
+#'
+#' @inheritParams helio.writeSchedule
+#'
+#' @return
+#' @export Heliospectra file with random disco
+#'
+#' @examples
+helio.disco = function(filename, format=c('csv', 'json')){
+  nEvents = helio.eventLimit
+
+  #make random matrix of light intensities as regime
+  random = sapply(1:nEvents, function(i){
+    sample(0:1000, size=8, replace=T)
+  })
+  refinedRandom = sapply(1:nEvents, function(i){ #need some LED channels off in each event otherwise it's too
+    pos = sample(c(1:8), size=4)
+    random[pos,i] = c(rep(0, 4))
+    random[,i]
+  })
+
+  # Get times
+  time_vec = seq(from=lubridate::hms('00:00:00'), by=lubridate::seconds(1), length.out=nEvents)
+  time_vec = as.POSIXct(time_vec, origin=lubridate::origin, tz='GMT')
+  time_mat = LightFitR::internal.makeTimes(time_vec)
+
+  # Make regime
+  regime = as.matrix(rbind(time_mat, refinedRandom, c(rep(0, nEvents)))) #make white light = 0 the whole time
+  rownames(regime) = c('time', 'hours', 'minutes', 'seconds', helio.dyna.leds[,'name'])
+  #image(regime) #sanity check that things are random
+
+  rm(random, refinedRandom, time_vec)
+
+  LightFitR::write.helioSchedule(regime, filename, format=format)
+}
