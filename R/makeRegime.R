f37b6329d3185459cb7329966119bb79dc1423cf
Gina Vong <gina121vong@gmail.com>
2024-08-19 10:47:32 +0100

    Create makeRegime.R

---
+++ R/makeRegime.R
@@ -0,0 +1,39 @@
+#' Create a regime (matrix) to program lights to achieve intended irradiances
+#'
+#' @inheritParams internal.calibCombine
+#' @inheritParams internal.makeTimes
+#' @inheritParams internal.closestIntensities
+#' @interitParams nnls_intensities
+#'
+#' @return Matrix with light regime needed to program the lights
+#' @export
+#'
+#' @examples
+makeRegime = function(timeVector_POSICx, irradiance_matrix, calibration_leds, calibration_wavelengths, calibration_intensities, calibration_irradiances, peaks=helio.dyna.leds$wavelength){
+
+  # Setup
+  calibrationDf = LightFitR::internal.calibCombine(calibration_leds, calibration_wavelengths, calibration_intensities, calibration_irradiances)
+
+  ## Checks
+
+  LightFitR::internal.checkNAs(irradiance_matrix)
+  LightFitR::helio.checkFormat(irradiance_matrix)
+  LightFitR::helio.checkWhite(irradiance_matrix)
+
+  LightFitR::checkRange(irradiance_matrix, calibrationDf$led, calibrationDf$wavelength, calibrationDf$intensity, calibrationDf$irradiance)
+
+  # Times
+  times = LightFitR::internal.makeTimes(timeVector_POSICx)
+
+  # Calculate intensities
+  closestIntensities = LightFitR::internal.closestIntensities(irradiance_matrix, calibrationDf, peaks=peaks)
+  intensities = LightFitR::nnls_intensities(irradiance_matrix, closestIntensities, calibrationDf$leds, calibrationDf$wavelength, calibrationDf$intensity, calibrationDf$irradiance, peaks=peaks)
+
+  # Make regime
+  regime = as.matrix(rbind(times, intensities))
+  rownames(regime) = c(rownames(times), LightFitR::helio.dyna.leds$name)
+  colnames(regime) = colnames(times)
+
+  return(regime)
+
+}
