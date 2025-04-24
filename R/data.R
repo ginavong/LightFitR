26c3809d95bc9eb150c82d8e2d9897a6d0d2d90f
Gina Vong <gina121vong@gmail.com>
2025-04-24 13:43:56 +0100

    Added example json schedule to demonstrate the import function

--- R/data.R
+++ R/data.R
@@ -44,7 +44,6 @@
 #' Matrix of random target irradiances for example purposes
 #'
 #' @format A matrix with 9 rows and 10 columns: each row represents an LED channel and each column represents an event
-#'}
 #'
 "target_irradiance"
 
@@ -62,6 +61,15 @@
 #' Example timepoints for events
 #'
 #' @format A vector of length 10 with timepoints in POSIXct format
-#'}
 #'
 "time_vector"
+
+#' example json schedule
+#'
+#' Example Heliospectra schedule in json format.This schedule was run to calibrate the lights.
+#'
+#' @format A .txt file with a Heliospectra schedule in the json format.
+#'
+#' @source https://github.com/ginavong/2024_LightFitR_MethodsPaper/blob/master/data/regimes/calibration/calibration.txt
+#'
+"example_json_schedule.txt"
