0218d7898b84d4725ece68930cf02e04e9359240
Gina Vong <gina121vong@gmail.com>
2025-06-17 17:58:06 +0100

    Fixed example_regime hopefully

--- data-raw/example_closest.R
+++ data-raw/example_closest.R
@@ -3,10 +3,10 @@
 # Import data
 target_irradiance = LightFitR::target_irradiance
 calib = LightFitR::calibration
-calib = LightFitR::internal.calibCombine(calib$led, calib$wavelength, calib$intensity, calib$irradiance)
+calib = LightFitR:::internal.calibCombine(calib$led, calib$wavelength, calib$intensity, calib$irradiance)
 
 # Make closest intensities
-example_closest = LightFitR::internal.closestIntensities(target_irradiance, calib)
+example_closest = LightFitR:::internal.closestIntensities(target_irradiance, calib)
 
 # Format
 rownames(example_closest) = LightFitR::helio.dyna.leds$name
--- data-raw/example_intensities.R
+++ data-raw/example_intensities.R
@@ -4,7 +4,7 @@
 target = LightFitR::target_irradiance
 calib = LightFitR::calibration[, c(3,5,4,6)]
 
-closest = LightFitR::internal.closestIntensities(target, calib)
+closest = LightFitR:::internal.closestIntensities(target, calib)
 
 example_intensities = LightFitR::nnls_intensities(target, closest, calib$led, calib$wavelength, calib$intensity, calib$irradiance, peaks=LightFitR::helio.dyna.leds$wavelength)
 
